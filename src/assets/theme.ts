9344c2c3b917ba3e02d30e555027831a7a5e2d89
ampoldellova <dellova36@gmail.com>
2025-04-11 14:46:29 +0800

    Navigation Bar

---
+++ src/assets/theme.ts
@@ -0,0 +1,8 @@
+const COLORS = {
+  primary: '#400e32',
+  secondary: '#A61F69',
+  tertiary: '#F2921D',
+  quarternary: '#F2CD5C',
+}
+
+export { COLORS }
---
+++ src/components/NavigationBar.vue
@@ -0,0 +1,31 @@
+<template>
+  <div
+    style="width: 100%; background-color: #400e32; height: 80px; display: flex; align-items: center"
+  >
+    <el-row style="padding-left: 2%; padding-right: 2%; width: 100%">
+      <el-col :span="12">
+        <el-tooltip class="box-item" effect="dark" content="Home Page" placement="bottom">
+          <el-image :src="logo" fit="cover" style="height: 60px; width: 100px" />
+        </el-tooltip>
+      </el-col>
+      <el-col :span="12" style="display: flex; align-items: center; justify-content: end">
+        <el-tooltip class="box-item" effect="dark" content="Shop" placement="bottom">
+          <el-button text :style="{ color: COLORS.quarternary }">Tindahan</el-button>
+        </el-tooltip>
+        <el-button :style="{ backgroundColor: COLORS.secondary, color: 'white' }" round>
+          Sign In
+        </el-button>
+        <el-button :style="{ backgroundColor: COLORS.secondary, color: 'white' }" round>
+          Register
+        </el-button>
+      </el-col>
+    </el-row>
+  </div>
+</template>
+
+<script setup lang="ts">
+import logo from '@/assets/TeksPogRush-Logo.png'
+import { COLORS } from '@/assets/theme'
+</script>
+
+<style scoped></style>
