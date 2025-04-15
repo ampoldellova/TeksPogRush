72004a5beee59e0c6e7849988d1224257c772972
ampoldellova <dellova36@gmail.com>
2025-04-15 17:47:14 +0800

    Home Page Animations

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -21,6 +21,7 @@
           justify-content: center;
           align-items: center;
           height: 100vh;
+          width: 100%;
           flex-direction: column;
         "
       >
@@ -29,37 +30,33 @@
             y: [0, -10, 0],
             transition: { duration: 2, repeat: Infinity, ease: 'easeInOut' },
           }"
-          style="
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            width: 30%;
-            height: auto;
-          "
+          style="display: flex; justify-content: center; align-items: center"
         >
-          <el-image :src="logo" fill="cover" />
+          <el-image :src="logo" fill="cover" style="width: 100%; height: 70vh" />
         </motion.div>
 
-        <motion.button
-          :whileHover="{
-            scale: 1.2,
-            transition: { duration: 0.3 },
-          }"
-          :whilePress="{ scale: 0.9 }"
-          :style="{
-            fontFamily: 'bold',
-            width: '10%',
-            backgroundColor: '#ffd200',
-            color: 'black',
-            borderWidth: 0,
-            height: '5%',
-            borderRadius: '15px',
-            cursor: 'pointer',
-            boxShadow: '5px 8px 0px 1px rgba(0, 0, 0, 1)',
-          }"
-        >
-          <RouterLink to="/play" style="color: black">S T A R T</RouterLink>
-        </motion.button>
+        <RouterLink to="/play">
+          <motion.button
+            :whileHover="{
+              scale: 1.2,
+              transition: { duration: 0.3 },
+            }"
+            :whilePress="{ scale: 0.9 }"
+            :style="{
+              fontFamily: 'bold',
+              width: '200px',
+              backgroundColor: '#ffd200',
+              color: 'black',
+              borderWidth: 0,
+              height: '50px',
+              borderRadius: '15px',
+              cursor: 'pointer',
+              boxShadow: '5px 8px 0px 1px rgba(0, 0, 0, 1)',
+            }"
+          >
+            S T A R T
+          </motion.button>
+        </RouterLink>
       </div>
     </div>
   </div>
