1cf60e3aa929244f9db44f4efa51cb76b1c4b2dc
ampoldellova <dellova36@gmail.com>
2025-04-15 16:37:05 +0800

    Revised Navbar

--- src/components/NavigationBar.vue
+++ src/components/NavigationBar.vue
@@ -2,11 +2,12 @@
   <div
     style="
       width: 100%;
-      background-color: #400e32;
+      background-color: transparent;
       height: 10vh;
       display: flex;
       align-items: center;
       justify-content: space-between;
+      position: absolute;
     "
   >
     <el-row style="padding-left: 2%; padding-right: 2%; width: 100%">
@@ -21,31 +22,31 @@
       </el-col>
 
       <el-col :span="8" class="responsive-button">
-        <el-button
-          @click="signInDialog = true"
-          size="large"
-          color="#A61F69"
-          :style="{
-            color: 'white',
-            fontFamily: 'regular',
-          }"
-          round
+        <el-text
+          :style="{ fontFamily: 'regular', fontSize: '18px', color: 'white', cursor: 'pointer' }"
         >
-          Sign In
-        </el-button>
-
-        <el-button
-          @click="registerDialog = true"
-          size="large"
-          color="#A61F69"
-          :style="{
-            color: 'white',
-            fontFamily: 'regular',
-          }"
-          round
+          Home
+        </el-text>
+        <el-text
+          :style="{ fontFamily: 'regular', fontSize: '18px', color: 'white', cursor: 'pointer' }"
+          >Play</el-text
+        >
+        <el-text
+          :style="{ fontFamily: 'regular', fontSize: '18px', color: 'white', cursor: 'pointer' }"
+          >Shop</el-text
+        >
+        <el-text
+          @click="signInDialog = true"
+          :style="{ fontFamily: 'regular', fontSize: '18px', color: 'white', cursor: 'pointer' }"
+        >
+          Login
+        </el-text>
+        <el-text
+          @click="registerNavButton"
+          :style="{ fontFamily: 'regular', fontSize: '18px', color: 'white', cursor: 'pointer' }"
         >
           Register
-        </el-button>
+        </el-text>
       </el-col>
 
       <el-col :span="8" class="responsive-menu">
@@ -107,19 +108,19 @@
 
       <el-row style="display: flex; justify-content: center; align-items: center; cursor: pointer">
         <el-icon :style="{ fontSize: '20px', marginRight: '10px', color: 'white' }">
-          <Coin />
+          <SwitchFilled />
         </el-icon>
         <el-text :style="{ fontFamily: 'regular', color: 'white', fontSize: '16px' }">
-          Tindahan
+          Play
         </el-text>
       </el-row>
 
       <el-row style="display: flex; justify-content: center; align-items: center; cursor: pointer">
         <el-icon :style="{ fontSize: '20px', marginRight: '10px', color: 'white' }">
-          <SwitchFilled />
+          <TakeawayBox />
         </el-icon>
         <el-text :style="{ fontFamily: 'regular', color: 'white', fontSize: '16px' }">
-          Play
+          Shop
         </el-text>
       </el-row>
 
@@ -143,6 +144,7 @@
       <el-text :style="{ fontFamily: 'bold', color: 'black' }">Email</el-text>
       <el-col :span="24">
         <el-input
+          :prefix-icon="Message"
           v-model="signInEmail"
           style="width: 100%"
           placeholder="Enter your email"
@@ -157,6 +159,7 @@
       </el-text>
       <el-col :span="24">
         <el-input
+          :prefix-icon="Lock"
           v-model="signInPassword"
           style="width: 100%"
           placeholder="Enter your password"
@@ -167,8 +170,9 @@
       </el-col>
     </el-row>
 
-    <el-row style="margin-top: 20px">
+    <el-row style="margin-top: 30px">
       <el-button
+        size="large"
         :style="{
           backgroundColor: COLORS.dark,
           fontFamily: 'semiBold',
@@ -227,6 +231,7 @@
       <el-text :style="{ fontFamily: 'bold', color: 'black' }">Username</el-text>
       <el-col :span="24">
         <el-input
+          :prefix-icon="User"
           v-model="username"
           style="width: 100%"
           placeholder="Enter your usename"
@@ -239,6 +244,7 @@
       <el-text :style="{ fontFamily: 'bold', color: 'black' }">Email</el-text>
       <el-col :span="24">
         <el-input
+          :prefix-icon="Message"
           v-model="email"
           style="width: 100%"
           placeholder="Enter your email"
@@ -251,6 +257,7 @@
       <el-text :style="{ fontFamily: 'bold', color: 'black' }">Contact</el-text>
       <el-col :span="24">
         <el-input
+          :prefix-icon="Phone"
           v-model="contact"
           style="width: 100%"
           placeholder="Enter your contact number"
@@ -265,6 +272,7 @@
       </el-text>
       <el-col :span="24">
         <el-input
+          :prefix-icon="Unlock"
           v-model="password"
           style="width: 100%"
           placeholder="Enter your password"
@@ -281,6 +289,7 @@
       </el-text>
       <el-col :span="24">
         <el-input
+          :prefix-icon="Lock"
           v-model="confirmPassword"
           style="width: 100%"
           placeholder="Confirm your password"
@@ -291,8 +300,9 @@
       </el-col>
     </el-row>
 
-    <el-row style="margin-top: 20px">
+    <el-row style="margin-top: 30px">
       <el-button
+        size="large"
         :style="{
           backgroundColor: COLORS.dark,
           fontFamily: 'semiBold',
@@ -303,6 +313,22 @@
       >
         Register
       </el-button>
+
+      <div
+        v-if="fromLogin"
+        @click="backDialogButton"
+        style="display: flex; gap: 10px; align-items: center; cursor: pointer"
+      >
+        <el-icon :style="{ fontSize: '16px', color: COLORS.dark, marginTop: '10px' }"
+          ><Back
+        /></el-icon>
+        <el-text
+          :style="{ fontFamily: 'regular', color: COLORS.dark, fontSize: '16px' }"
+          style="margin-top: 10px"
+        >
+          Go back
+        </el-text>
+      </div>
     </el-row>
   </el-dialog>
 </template>
@@ -311,14 +337,21 @@
 import logo from '@/assets/TeksPogRush-Logo-small.png'
 import { COLORS } from '@/assets/theme'
 import {
+  Back,
   Close,
   Coin,
   CreditCard,
   HomeFilled,
   House,
+  Lock,
   Menu,
+  Message,
+  Phone,
   Switch,
   SwitchFilled,
+  TakeawayBox,
+  Unlock,
+  User,
 } from '@element-plus/icons-vue'
 import { onMounted, onUnmounted, ref } from 'vue'
 import { useRouter } from 'vue-router'
@@ -327,6 +360,7 @@ const router = useRouter()
 
 const drawer = ref(false)
 
+const fromLogin = ref(false)
 const signInDialog = ref(false)
 const registerDialog = ref(false)
 
@@ -339,9 +373,20 @@ const contact = ref('')
 const password = ref('')
 const confirmPassword = ref('')
 
+const registerNavButton = () => {
+  fromLogin.value = false
+  registerDialog.value = true
+}
+
 const registerDialogButton = () => {
   signInDialog.value = false
+  fromLogin.value = true
   registerDialog.value = true
+}
+
+const backDialogButton = () => {
+  registerDialog.value = false
+  signInDialog.value = true
 }
 
 const resetPasswordDialogButton = () => {
@@ -379,6 +424,7 @@ onUnmounted(() => {
   display: flex;
   align-items: center;
   justify-content: end;
+  gap: 40px;
 }
 
 .responsive-menu {
--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,26 +1,26 @@
 <template>
-  <div :style="{ backgroundColor: COLORS.dark, height: '90vh', width: '100%' }">
-    <el-container>
-      <!-- <el-aside width="15vw" :style="{ backgroundColor: COLORS.secondary, height: '90vh' }">
-      </el-aside> -->
-      <!-- <el-main width="85vw" :style="{ height: '90vh' }">
-        <el-row>
-          <el-col :span="24">
-            <el-image
-            :src="sunburst"
-            fill="fill"
-            style="width: 100%; height: 30vh; border-radius: 15px"
-            />
-          </el-col>
-        </el-row>
-      </el-main> -->
-    </el-container>
+  <div
+    :style="{
+      backgroundImage: `url(${backgrounds})`,
+      backgroundSize: 'cover',
+      backgroundPosition: 'center',
+      height: '100vh',
+      width: '100%',
+    }"
+  >
+    <div
+      :style="{
+        background: 'linear-gradient(to bottom, black, transparent)',
+        height: '100vh',
+        width: '100%',
+      }"
+    ></div>
   </div>
 </template>
 
 <script setup lang="ts">
-// import sunburst from '@/assets/sunburst.png'
 import { COLORS } from '@/assets/theme'
+import backgrounds from '@/assets/Background.png'
 </script>
 
 <style scoped></style>
