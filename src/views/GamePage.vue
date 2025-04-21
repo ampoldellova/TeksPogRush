4fbce81c24586eb247519e864c8b9a069dd32862
ampoldellova <dellova36@gmail.com>
2025-04-21 16:54:08 +0800

    Shop page

--- src/views/GamePage.vue
+++ src/views/GamePage.vue
@@ -1,73 +1,73 @@
-
 <template>
-    <div class="backgroundGameImage">
+  <div class="backgroundGameImage">
+    <el-row>
+      <div class="shadowOverlay"></div>
+      <el-col :span="8">
+        <img
+          src="../assets/TeksPogRush-Logo-small.png"
+          alt=""
+          style="height: 10vh; margin-left: 15px"
+        />
+        <div class="totalBet"></div>
+        <div class="balance"></div>
+      </el-col>
+      <el-col :span="8">
+        <div class="arenaTitleImage">
+          <img src="../assets/Arena1.png" alt="Arena 1" />
+        </div>
+        <div class="backgroundGameTable">
+          <img
+            src="../assets/backgroundGameTable.jpg"
+            alt="Game Table"
+            style="max-height: 400px; width: 100vw; border-radius: 80px"
+          />
+        </div>
+        <div class="betContainer"></div>
         <el-row>
-            <div class="shadowOverlay"></div>
-            <el-col :span="8">
-                <img src="../assets/TeksPogRush-Logo-small.png" alt="" style="height: 10vh; margin-left: 15px;">
-                <div class="totalBet"></div>
-                <div class="balance"></div>
-             </el-col>
-            <el-col :span="8">
-                <div class="arenaTitleImage">
-                    <img src="../assets/Arena1.png" alt="Arena 1">
-                </div>
-                <div class="backgroundGameTable">
-                    <img src="../assets/backgroundGameTable.jpg" alt="Game Table" style="max-height: 400px; width: 100vw; border-radius: 80px;">
-                </div>
-                <div class="betContainer">
-                </div>
-                <el-row>
-                    <el-col class="betMoney">
-
-                    </el-col>
-                </el-row>
-            </el-col>
-            <el-col :span="8"></el-col>
+          <el-col class="betMoney"> </el-col>
         </el-row>
-    </div>
+      </el-col>
+      <el-col :span="8"></el-col>
+    </el-row>
+  </div>
 </template>
 
-<script setup lang="ts">
-
-</script>
+<script setup lang="ts"></script>
 
 <style scoped>
-
-
-*{
-    overflow: hidden;
+* {
+  overflow: hidden;
 }
 
 .titleImageContainer {
-    margin-left: 30px;
+  margin-left: 30px;
 }
 
 .backgroundGameImage {
-    background-image: url('../assets/BackgroundImage.png');
-    background-size: cover;
-    background-position: center;
-    width: 100%;
-    height: 100vh;
+  background-image: url('../assets/BackgroundImage.png');
+  background-size: cover;
+  background-position: center;
+  width: 100%;
+  height: 100vh;
 }
 
-.totalBet{
-    margin-left: 11vh;
-    margin-top: 62.2vh;
-    width: 20vw;
-    border-radius: 10px;
-    background-color: #D9D9D9;
-    opacity: 0.5;
+.totalBet {
+  margin-left: 11vh;
+  margin-top: 62.2vh;
+  width: 20vw;
+  border-radius: 10px;
+  background-color: #d9d9d9;
+  opacity: 0.5;
 }
 
-.balance{
-    margin-left: 2vh;
-    margin-top: 3vh;
-    width: 20vw;
-    border-radius: 10px;
-    background-color: black;
-    opacity: 0.5;
-    /* border: 20px solid blue; */
+.balance {
+  margin-left: 2vh;
+  margin-top: 3vh;
+  width: 20vw;
+  border-radius: 10px;
+  background-color: black;
+  opacity: 0.5;
+  /* border: 20px solid blue; */
 }
 
 .shadowOverlay {
@@ -80,38 +80,36 @@
   background: linear-gradient(to bottom, rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0));
 }
 
-
-.el-col{
-     height: 100vh;
+.el-col {
+  height: 100vh;
 }
 
-.arenaTitleImage{
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    margin-bottom: 70px;
-    margin-top: 20px;
+.arenaTitleImage {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-bottom: 70px;
+  margin-top: 20px;
 }
 
-.backgroundGameTable{
-    display: flex;
-    justify-content: center;
-    align-items: center;
+.backgroundGameTable {
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
 
-.betContainer{
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    height: 20vh;
-    margin-top: 40px;
-    border-radius: 20px;
-    background-color: #D9D9D9;
-    filter: drop-shadow();
+.betContainer {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 20vh;
+  margin-top: 40px;
+  border-radius: 20px;
+  background-color: #d9d9d9;
+  filter: drop-shadow();
 }
 
-.totalBet{
-    height: 59px;
-
+.totalBet {
+  height: 59px;
 }
 </style>
--- src/views/ShopPage.vue
+++ src/views/ShopPage.vue
@@ -7,20 +7,110 @@
       display: 'flex',
       justifyContent: 'center',
       alignItems: 'center',
+      flexDirection: 'column',
     }"
   >
-    <PogShopCard1 />
-    <PogShopCard2 />
-    <PogShopCard3 />
-    <PogShopCard4 />
+    <el-row
+      :gutter="20"
+      style="width: 100%; justify-content: center; align-items: center; margin-bottom: 20px"
+    >
+      <el-col :span="4.5">
+        <el-button
+          size="large"
+          :style="{
+            width: '100%',
+            backgroundColor: COLORS.dark,
+            borderRadius: '15px',
+            fontFamily: 'bold',
+            color: 'white',
+            borderColor: '#2a2c32',
+          }"
+          >Common
+        </el-button>
+      </el-col>
+      <el-col :span="4.5">
+        <el-button
+          size="large"
+          :style="{
+            width: '100%',
+            backgroundColor: COLORS.dark,
+            borderRadius: '15px',
+            fontFamily: 'bold',
+            color: 'white',
+            borderColor: '#2a2c32',
+          }"
+          >Rare
+        </el-button>
+      </el-col>
+      <el-col :span="4.5">
+        <el-button
+          size="large"
+          :style="{
+            width: '100%',
+            backgroundColor: COLORS.dark,
+            borderRadius: '15px',
+            fontFamily: 'bold',
+            color: 'white',
+            borderColor: '#2a2c32',
+          }"
+          >Epic
+        </el-button>
+      </el-col>
+      <el-col :span="4.5">
+        <el-button
+          size="large"
+          :style="{
+            width: '100%',
+            backgroundColor: COLORS.dark,
+            borderRadius: '15px',
+            fontFamily: 'bold',
+            color: 'white',
+            borderColor: '#2a2c32',
+          }"
+          >Legendary
+        </el-button>
+      </el-col>
+      <el-col :span="4">
+        <el-input
+          v-model="search"
+          :style="{ width: '100%', backgroundColor: '#C0C0C0', borderRadius: '15px' }"
+          placeholder="Search..."
+          :input-style="{ fontFamily: 'regular', color: COLORS.dark }"
+          :prefix-icon="Search"
+        />
+      </el-col>
+      <el-col :span="2">
+        <el-button
+          size="large"
+          :icon="Sort"
+          circle
+          :style="{
+            backgroundColor: COLORS.dark,
+            color: 'white',
+            borderColor: '#2a2c32',
+          }"
+        />
+      </el-col>
+    </el-row>
+    <div style="width: 100%; display: flex; justify-content: center; align-items: center">
+      <PogShopCard1 />
+      <PogShopCard2 />
+      <PogShopCard3 />
+      <PogShopCard4 />
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
+import { COLORS } from '@/assets/theme'
 import PogShopCard1 from '@/components/PogShopCard1.vue'
 import PogShopCard2 from '@/components/PogShopCard2.vue'
 import PogShopCard3 from '@/components/PogShopCard3.vue'
 import PogShopCard4 from '@/components/PogShopCard4.vue'
+import { Search, Sort } from '@element-plus/icons-vue'
+import { ref } from 'vue'
+
+const search = ref('')
 </script>
 
 <style scoped></style>
