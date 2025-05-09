b2b1ade96f30d5a2b3a9848e2b6e00da648fe1d0
James <James.Llanos@tbu.net>
2025-05-09 11:36:57 +0800

    added feature 'acticityLOgs' by chado

--- src/views/PlayPage.vue
+++ src/views/PlayPage.vue
@@ -25,6 +25,54 @@
     </div>
   </div>
 
+  <!-- Player activity log -->
+  <div v-if="windowWidth > 980" class="wallet-balance">
+    <p>Wallet Balance: ₱{{ walletStore.userWalletBalance }}</p>
+    <p v-if="Pog1BetDisplay">Bet on Pog1: ₱{{ Pog1BetDisplay }}</p>
+    <p v-if="EqualizerBetDisplay">Bet on Equalizer: ₱{{ EqualizerBetDisplay }}</p>
+    <p v-if="Pog2BetDisplay">Bet on Pog2: ₱{{ Pog2BetDisplay }}</p>
+    <hr />
+    <p>
+      <strong>Total Bet: ₱{{ Pog1BetDisplay + EqualizerBetDisplay + Pog2BetDisplay }}</strong>
+    </p>
+  </div>
+
+  <el-col :span="8" class="responsive-menu" v-if="windowWidth <= 980">
+    <!-- <el-icon><Tickets /></el-icon> -->
+    <el-button
+      plain
+      @click="dialogVisible = true"
+      size="large"
+      color="#A61F69"
+      :icon="Tickets"
+      circle
+    />
+  </el-col>
+
+  <el-dialog
+    v-model="dialogVisible"
+    width="260"
+    class="wallet-balance2"
+    v-if="windowWidth <= 980"
+    style="
+      max-height: 500px;
+      border-radius: 25px;
+      text-align: center;
+      background-color: rgba(0, 0, 0, 0.75);
+    "
+  >
+    <div class="wallet-balance2">
+      <p>Activity Log:</p>
+      <br />
+      <p>Wallet Balance: ₱{{ walletStore.userWalletBalance }}</p>
+      <p v-if="Pog1BetDisplay">POG1: ₱{{ Pog1BetDisplay }}</p>
+      <p v-if="EqualizerBetDisplay">EQUALIZER: ₱{{ EqualizerBetDisplay }}</p>
+      <p v-if="Pog2BetDisplay">POG2: ₱{{ Pog2BetDisplay }}</p>
+      <hr />
+      <p>Total Bet: ₱{{ Pog1BetDisplay + EqualizerBetDisplay + Pog2BetDisplay }}</p>
+    </div>
+  </el-dialog>
+
   <WinHistory />
 
   <BetDialog
@@ -98,7 +146,7 @@ import pog1Win from '@/assets/play/pog1Win.png'
 import pog2Win from '@/assets/play/pog2Win.png'
 import equalizerWin from '@/assets/play/equalizerWin.png'
 
-import { reactive, ref, onMounted, watch, onUnmounted } from 'vue'
+import { reactive, ref, onMounted, watch, onUnmounted, onBeforeUnmount } from 'vue'
 import { ElMessage, type ButtonInstance } from 'element-plus'
 import { useRouter } from 'vue-router'
 
@@ -114,6 +162,7 @@ import { useWinHistoryStore } from '../stores/winHistoryStore'
 import type { Bet } from '@/components/models/types'
 import WinHistory from '@/components/Play/WinHistory.vue'
 import { MaxBetMessage } from '@/components/composables/useGlobalUtils'
+// import { Menu } from 'element-plus/icons'
 
 const userStore = useAuthenticationStore()
 const walletStore = useWalletStore()
@@ -481,6 +530,7 @@ if (totalBet > 0) {
 }
 
 import { NoBetUndoMessage, NoBetToClearMessage } from '@/components/composables/useGlobalUtils'
+import { Tickets } from '@element-plus/icons-vue'
 const NoBetToUndo = NoBetUndoMessage
 const NoBetToClear = NoBetToClearMessage
 
@@ -700,20 +750,79 @@ onUnmounted(() => {
   intervals.value.forEach(clearInterval)
   console.log('Game stopped when leaving page.')
 })
+
+const dialogVisible = ref(false)
+
+const windowWidth = ref(window.innerWidth)
+
+const updateWindowWidth = () => {
+  windowWidth.value = window.innerWidth
+}
+
+onMounted(() => {
+  window.addEventListener('resize', updateWindowWidth)
+})
+
+onBeforeUnmount(() => {
+  window.removeEventListener('resize', updateWindowWidth)
+})
 </script>
 
 <style scoped>
 .wallet-balance {
-  position: absolute;
-  bottom: 10px;
-  left: 10px;
-  width: 230px;
+  position: fixed;
+  bottom: 20px;
+  left: 20px;
+  max-width: 260px;
   color: white;
-  font-size: 20px;
+  font-size: 15px;
   font-weight: bold;
   border: 2px solid white;
   border-radius: 10px;
-  padding: 10px;
-  background-color: rgba(122, 129, 129, 0.6);
+  padding: 16px 20px;
+  line-height: 1.5;
+  background-color: rgba(0, 0, 0, 0.75);
+  z-index: 2000;
+}
+
+.wallet-balance2 {
+  margin: auto;
+  max-width: 260px;
+  color: white;
+  font-size: 15px;
+  font-weight: bold;
+  border: 2px solid white;
+  border-radius: 10px;
+  padding: 16px 20px;
+  line-height: 1.5;
+  background-color: rgba(0, 0, 0, 0.75);
+  z-index: 100;
+  text-align: center;
+}
+
+.wallet-balance p {
+  margin: 4px 0;
+}
+
+.wallet-balance hr {
+  margin: 10px 0;
+  border: 0;
+  height: 1px;
+  background: #ffffff40;
+}
+
+.responsive-menu {
+  display: none;
+  position: absolute;
+  bottom: 0;
+  left: 0;
+}
+
+@media (max-width: 980px) {
+  .responsive-menu {
+    display: flex;
+    align-items: center;
+    justify-content: flex-start;
+  }
 }
 </style>
